13925aac9d58f1e1ed737961332cd28b0d664ce6
dominhit215@gmail.com <dominhit215@gmail.com>
2024-11-15 03:17:54 +0700

    hiểu về morgan, helmet, compression

--- ecommerce-backend-nodejs/src/app.js
+++ ecommerce-backend-nodejs/src/app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const compression = require('compression')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
 const app = express()
+
+// init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+// init db
+
+
+// init routes
+
 
 
 module.exports = app
