9a64e81bcdcd7107a932db8a1319ea137fa6e1d0
dominhit215@gmail.com <dominhit215@gmail.com>
2024-12-06 16:49:25 +0700

    connect db mongo ver củ chuối và ver PRO apply Singleton Patter

--- ecommerce-backend-nodejs/src/app.js
+++ ecommerce-backend-nodejs/src/app.js
@@ -9,9 +9,17 @@ app.use(morgan("dev"))
 app.use(helmet())
 app.use(compression())
 // init db
-
+require('./dbs/init.mongodb.v1')
 
 // init routes
+app.get("/", (req, res, next) => {
+    return res.status(500).json({
+        message: "Welcome"
+    })
+})
+
+
+// handling error
 
 
 
