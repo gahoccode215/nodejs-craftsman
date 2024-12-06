31afecbdc4bec5c2b0754325799643c8f688e2f1
dominhit215@gmail.com <dominhit215@gmail.com>
2024-12-06 17:25:17 +0700

    cấu hình .env

--- ecommerce-backend-nodejs/src/app.js
+++ ecommerce-backend-nodejs/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+require('dotenv').config()
 const compression = require('compression')
 const { default: helmet } = require('helmet')
 const morgan = require('morgan')
 const app = express()
+
+//console.log(`Process::`, process.env)
 
 // init middlewares
 app.use(morgan("dev"))
@@ -11,7 +14,7 @@ app.use(compression())
 // init db
 require('./dbs/init.mongodb.v2')
 const {checkOverload} = require("./helpers/check.connect")
-checkOverload()
+//checkOverload()
 // init routes
 app.get("/", (req, res, next) => {
     return res.status(500).json({
